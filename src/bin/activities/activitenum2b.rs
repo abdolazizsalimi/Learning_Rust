a61d3509f9ae7e064b98547e9c11f9de0d9dddba
abdolazizsalimi <aziz.s6882@gmail.com>
2023-05-15 11:37:17 +0430

    add :[activite num 2:b  for control flow ]

---
+++ src/bin/activities/activitenum2b.rs
@@ -0,0 +1,12 @@
+
+
+fn main(){
+    let num = 4;
+    if num > 5 {
+        println!("num > 5");
+    }else if num < 5 {
+        println("num < 5");
+    }else {
+        println!("num == 5");
+    }
+}
