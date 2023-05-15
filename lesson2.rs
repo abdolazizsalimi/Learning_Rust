426bcdc3c9ead2090773488a828a36ac2e5da3d5
abdolazizsalimi <aziz.s6882@gmail.com>
2023-05-15 10:28:12 +0430

    add :[lesson 2 ]

---
+++ lesson2.rs
@@ -0,0 +1,24 @@
+// subtract function 
+fn sub(a: i32 ,b: i32) -> i32{
+    a-b;
+}
+
+// main function our entry point f the application 
+fn main(){
+
+    let sum = 2 + 3;
+    let value = 10 - 5;
+    let division = 10 / 2;
+    let multiply  = 2 * 7;
+    let five = sub(8,3);
+
+    // rem = 0  
+    let rem = 6 % 3;
+
+    // rem1 = 1  
+    let rem1 = 6 % 5;
+    
+    // rem2 = 2 
+    let rem2 = 6 % 4;
+
+}
